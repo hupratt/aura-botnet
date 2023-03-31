17921e80dd2c8d8c7b18314056fa6f586da437ea
hupratt <hprattdo@gmail.com>
2023-04-01 01:46:55 +0200

    adding botcommand to the admin

--- aura-server/convey/admin.py
+++ aura-server/convey/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import Bot, Command, File, Bot_Command
 
+
 class FileInline(admin.StackedInline):
     model = File
     extra = 1
-# class Bot_Command(admin.ModelAdmin):
-#     inlines = [
-#         FileInline,
-#     ]
+
+
+class BotCommand(admin.ModelAdmin):
+    inlines = ["Bot_Command"]
 
 
 class CommandAdmin(admin.ModelAdmin):
@@ -16,7 +17,8 @@ class CommandAdmin(admin.ModelAdmin):
         FileInline,
     ]
 
+
 # Register your models here.
 admin.site.register(Bot)
 admin.site.register(Command, CommandAdmin)
-# admin.site.register(Bot_Command)
+admin.site.register(Bot_Command)
